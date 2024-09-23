148ce14d3a9ab1321fa31da8cf66e6fff8ecd611
Dmitriy <d.krimin@gmail.com>
2024-09-23 14:02:50 +0300

    fixed

--- task3/src/Main.kt
+++ task3/src/Main.kt
@@ -1,16 +1,30 @@
 fun main() {
     println("task#1")
+    println("task#1")
 
 
+    val collection: Map<String, Any> = mapOf("name" to "Дмитрий", "age" to 41, "fn" to 28)
+    for ((key, value) in collection) {
+        println("$key: $value")
+    }
 
 
-    println("Введите Количество товара")
-    var x = readLine()!!.toInt()
-    var y: int
-    if (x > 0 && x <= 9) {
-        y = x * 1000
-    } elseif (x <= 19) { y = x * 800 } else { y = x * 600 }
-    println("Стоимость товаров: $x")
-
-
+    /*
+        println("task#2")
+        println("Введите свой возраст")
+        var x = readLine()!!.toInt()
+        println("Есть ли у Вас билет? (0 - нет, 1 - да)")
+        var y = readLine().equals("1")
+        println("Прошли ли Вы регистрацию? (0 - нет, 1 - да)")
+        var z = readLine().equals("1")
+        println("Есть ли у Вас VIP -статус? (0 - нет, 1 - да)")
+        var v = readLine().equals("1")
+        if (x >= 18 && y && z) {
+            println("Добро пожаловать на наше мероприятие!!")
+        } else if (y && v) {
+            println("Добро пожаловать на наше мероприятие дорогой гость!!")
+        } else {
+            println("Свободен холоп!!!")
+        }
+    */
 }
--- task4/src/Main.kt
+++ task4/src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    println("task#1")
+
+
+
+
+    println("Введите Количество товара")
+    var x = readLine()!!.toInt()
+    var y: Int
+    if (x > 0 && x <= 9) {
+        y = x * 1000
+    } else if (x <= 19) { y = x * 800 } else { y = x * 600 }
+    println("Стоимость товаров: $y")
+
+
 }
